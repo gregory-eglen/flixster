5283675c144f271682d6a241ce0c37b4dcf69f44
Gregory Eglen <eglen.g@gmail.com>
2019-08-23 04:52:02 +0000

    added sections to instructor namespace

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#index'
   namespace :instructor do
-    resources :courses, only: [:new, :create, :show]
+    resources :courses, only: [:new, :create, :show] do
+      resources :sections, only: [:new, :create]
+    end
   end
   resources :courses, only: [:index, :show]
 end
