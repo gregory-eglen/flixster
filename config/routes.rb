9c0403511a17004cc3504fce043e553e64d84f98
Gregory Eglen <eglen.g@gmail.com>
2019-09-06 02:38:30 +0000

    added dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resource :dashboard, only: [:show]
   root 'static_pages#index'
   resources :lessons, only: [:show]
   namespace :instructor do
